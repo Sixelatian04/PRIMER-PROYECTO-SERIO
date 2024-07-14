f0f4c67b47aea816945bb173206866bff8a0852c
Sixelatian04 <106364847+Sixelatian04@users.noreply.github.com>
2024-07-14 18:30:41 -0400

    Create menu.blade.php

---
+++ menu.blade.php
@@ -0,0 +1,19 @@
+<!-- need to remove -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <li class="nav-item">
+        <a href="{{ route('home') }}" class="nav-link {{ Request::is('home') ? 'active' : '' }}">
+            <i class="nav-icon fas fa-home"></i>
+            <p>Dashboard</p>
+        </a>
+    </li>
+
+</body>
+</html>
