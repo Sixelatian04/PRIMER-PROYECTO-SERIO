5dfbb81443bed55263a1930b3cf20d032f1390dd
Sixelatian04 <106364847+Sixelatian04@users.noreply.github.com>
2024-07-14 18:31:28 -0400

    Create home.blade.php

---
+++ home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admindek | Admin templeate</title>
+</head>
+<body>
+    @extends('layouts.app')
+
+    @section('content')
+        <div class="container-fluid">
+            <h1 class="text-black-50">Haz iniciado sesion</h1>
+        </div>
+    @endsection
+
+    @section('body')
+        <div class="container-body">
+            <header class="container-body__header">
+
+            </header>
+        </div>
+    @endsection
+</body>
+</html>
